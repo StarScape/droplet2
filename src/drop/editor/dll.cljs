f31889fe9f86ee265d5c1c3552a88c597cd7390d
Jack <mail@jackarrington.com>
2020-11-11 19:01:38 -0500

    Insert-before, and cleanup for insert-after.

--- src/drop/editor/dll.cljs
+++ src/drop/editor/dll.cljs
@@ -1,21 +1,20 @@
 (ns drop.editor.dll
+  ;; TODO: more detailed description
   "A fully-persistent, UUID-based doubly-linked list. Used for the list of paragraphs."
-  (:refer-clojure :exclude [last next])
-  )
+  (:refer-clojure :exclude [last next]))
 
 ;;(declare first)
 ;;(declare last)
 ;;(declare next)
 ;;(declare prev)
 (declare insert-after)
-;;(declare insert-before)
+(declare insert-before)
+
 ;; entries-map :: Map (UUID -> DLLEntry)
 ;; DLLEntry :: {uuid, prev-uuid, next-uuid, value}
 
-;; TODO: maybe just make it work for anything that has a :uuid property? Dynamic typing FTW.
-
 (deftype Node [^obj value ^string prev-uuid ^string next-uuid]
-  ;; Implemented pretty-printing for easier debugging
+  ;; Implement pretty-printing for easier debugging
   IPrintWithWriter
   (-pr-writer [n writer opts]
    (-write writer "#Node{value: ") (-write writer (.-value n))
@@ -23,6 +22,8 @@
    (-write writer ", next-uuid: ") (-write writer (.-next-uuid n))
    (-write writer "}")))
 
+;; TODO: make ISeqable
+;; TODO: make map and filterable - looks like (map) at least just uses first and rest
 (deftype DoublyLinkedList [entries-map first-uuid last-uuid]
   ISeq
   (-first [^DoublyLinkedList dll] (get (.-entries-map dll) (.-first-uuid dll)))
@@ -49,14 +50,7 @@
                      (= uuid (:uuid (.-value last))) (get new-entries (.-prev-uuid last)))]
       (DoublyLinkedList. new-entries new-first new-last))))
 
-(defn dll
-  "Constructor for a new doubly-linked-list."
-  ([]
-   (DoublyLinkedList. {} nil nil))
-  ([& xs]
-   (reduce (fn [dll x] (conj dll x)) (dll) xs)))
-
-(defn assoc-node [^Node node & kvs]
+(defn- assoc-node [^Node node & kvs]
   (reduce (fn [^Node new-node [k v]]
             (case k
               :value (Node. v (.-prev-uuid new-node) (.-next-uuid new-node))
@@ -64,26 +58,59 @@
               :next-uuid (Node. (.-value new-node) (.-prev-uuid new-node) v)))
           node (partition 2 kvs)))
 
+(defn- insert-between
+  "Inserts `val` into the map of `entries` immediately between `prev-uuid` and `next-uuid`.
+   If you call this with values of `prev-uuid` and `next-uuid` that are not adjacent, you
+   will die a horrible death.
+
+   Also note that this method operates on the entries-map, NOT the DLL itself."
+  [entries {new-uuid :uuid :as val} prev-uuid next-uuid]
+  (cond-> entries
+    (some? prev-uuid) (update prev-uuid assoc-node :next-uuid new-uuid)
+    (some? next-uuid) (update next-uuid assoc-node :prev-uuid new-uuid)
+    :always (assoc new-uuid (Node. val prev-uuid next-uuid))))
+
 ;; TODO: add condition that dll cannot be empty.
 (defn insert-after
-  "Inserts `val` into the double-linked list `dll`
-   immediately after the node with uuid = `prev-uuid`."
+  "Inserts `val` into the double-linked list `dll` immediately after the node with uuid = `prev-uuid`."
   [^DoublyLinkedList dll prev-uuid val]
   (let [next-uuid (.-next-uuid (get (.-entries-map dll) prev-uuid))
-        new-uuid (:uuid val)
-        new-last (if (= prev-uuid (.-last-uuid dll))
-                   new-uuid
-                   (.-last-uuid dll))
-        new-entries (cond-> (.-entries-map dll)
-                      :always           (update prev-uuid assoc-node :next-uuid new-uuid)
-                      :always           (assoc new-uuid (Node. val prev-uuid next-uuid))
-                      (some? next-uuid) (update next-uuid assoc-node :prev-uuid next-uuid))]
-    (DoublyLinkedList. new-entries (.-first-uuid dll) new-last)))
+        new-entries (insert-between (.-entries-map dll) val prev-uuid next-uuid)
+        new-last-uuid (if (= (.-last-uuid dll) prev-uuid)
+                        (:uuid val)
+                        (.-last-uuid dll))]
+    (DoublyLinkedList. new-entries (.-first-uuid dll) new-last-uuid)))
 
-(def l (dll {:uuid "1" :content "foo"} {:uuid "2" :content "bar"}))
-(get (.-entries-map l) "1")
-(get (.-entries-map l) "2")
+(defn insert-before
+  "Inserts `val` into the double-linked list `dll` immediately before the node with uuid = `prev-uuid`."
+  [^DoublyLinkedList dll next-uuid val]
+  (let [prev-uuid (.-prev-uuid (get (.-entries-map dll) next-uuid))
+        new-entries (insert-between (.-entries-map dll) val prev-uuid next-uuid)
+        new-first-uuid (if (= (.-first-uuid dll) next-uuid)
+                         (:uuid val)
+                         (.-first-uuid dll))]
+    (DoublyLinkedList. new-entries new-first-uuid (.-last-uuid dll))))
 
+(defn dll
+  "Constructor for a new doubly-linked-list."
+  ([]
+   (DoublyLinkedList. {} nil nil))
+  ([& xs]
+   (reduce (fn [dll x] (conj dll x)) (dll) xs)))
+
+(def l (dll {:uuid "1" :content "foo"} {:uuid "2" :content "bar"} {:uuid "3" :content "bizz"} {:uuid "5" :content "bang"}))
+(def l1 (insert-before l "5" {:uuid "4" :content "bar"}))
+(def l2 (insert-before l "1" {:uuid "-1" :content "pre"}))
+
+(.-entries-map l1)
+(.-last-uuid l1)
+(.-first-uuid l1)
+
+(.-entries-map l2)
+(.-last-uuid l2)
+(.-first-uuid l2)
+
+(.log js/console (insert-before l "5" {:uuid "4" :content "bar"}))
 
 ;; (first (dll {:uuid 1, :content "foo"}))
 ;; (assoc (dll) 1 {:uuid 1, :content "foo"})
