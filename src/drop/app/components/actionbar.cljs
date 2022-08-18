d28155dc0f75c3280fa0cb5af2bf483cc8b57063
Jack Arrington <mail@jackarrington.com>
2022-08-17 20:19:13 -0400

    Add animations for fade-in and fade out for actionbar buttons

--- src/drop/app/components/actionbar.cljs
+++ src/drop/app/components/actionbar.cljs
@@ -1,20 +1,27 @@
 (ns drop.app.components.actionbar
   (:require [drop.utils :refer [debounce]]
-            [reagent.core :as r :refer-macros [with-let]]))
+            [reagent.core :as r :refer-macros [with-let]]
+            ["@headlessui/react" :refer [Transition]]))
 
-(def actionbar-fade-out-ms 1000)
+(def actionbar-fade-out-ms 2500)
 
 (defn spacer []
   [:div {:class "w-1.5"}])
 
 (defn button [img-url active? transparent-mode? on-click]
-  [:button {:on-mouse-down #(.preventDefault %) ; prevent losing focus
-            :on-click on-click
-            :class ["m-0.5" "p-2" "rounded-md"
-                    (if active? "bg-light-blue" "bg-white")
-                    (if (and transparent-mode? (not active?)) "hidden" "")]}
-   [:img {:src img-url
-          :style {:width "15px"}}]])
+  [:> Transition {:show (boolean (or active? (not transparent-mode?)))
+                  :enter "transition-opacity duration-150"
+                  :enterFrom "opacity-0"
+                  :enterTo "opacity-100"
+                  :leave "transition-opacity duration-150"
+                  :leaveFrom "opacity-100"
+                  :leaveTo "opacity-0"}
+   [:button {:on-mouse-down #(.preventDefault %) ; prevent losing focus
+             :on-click on-click
+             :class ["m-0.5" "p-2" "rounded-md"
+                     (if active? "bg-light-blue" "bg-white")]}
+    [:img {:src img-url
+           :style {:width "15px"}}]]])
 
 (defn- invisible-button []
   [:div.invisible [button "icons/italic.svg" false false #()]])
@@ -36,7 +43,7 @@
                                   (reset! *transparent-mode? false)
                                   (.removeEventListener js/window "mousemove" move-handler)))))]
     (let [transparent? @*transparent-mode?
-          base-classes "fixed bottom-0 w-screen px-1 py-1 flex "]
+          base-classes "fixed bottom-0 w-screen px-1 py-1 flex transition-all duration-150 "]
       [:div {:class (str base-classes (if transparent?
                                         "bg-transparent"
                                         "bg-white border-t border-gray-200"))}
