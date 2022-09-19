085bc8263099c767ee9d2d118abb1a93d46aaaeb
Jack Arrington <mail@jackarrington.com>
2022-09-18 22:05:23 -0400

    Mega basic conversion of HTML to .drop--just convert every element into a paragraph with the paragraphs sole run set to the HTML element's innerText

--- src/slate/filetypes/html.cljs
+++ src/slate/filetypes/html.cljs
@@ -1,7 +1,24 @@
 (ns slate.filetypes.html
-  (:require-macros [slate.macros :refer [slurp-file]]))
+  (:require-macros [slate.macros :refer [slurp-file]])
+  (:require [slate.model.doc :as doc :refer [document]]
+            [slate.model.paragraph :as p :refer [paragraph]]
+            [slate.model.run :as r :refer [run]]))
 
-(comment
-  (slurp-file "README.md")
-  )
+(def test-file (slurp-file "test_files/html/the_quiet_universe.html"))
 
+(defn- str->document
+  [html-str]
+  (.parseFromString (js/DOMParser.) html-str "text/html"))
+
+(defn html->doc
+  "Converts an HTML string to a Droplet document."
+  [html-str]
+  (let [dom (str->document html-str)
+        html-elem->para #(paragraph [(run (.-innerText #p %))])
+        body-contents (js/Array.from (.. dom -body -children))
+        paragraphs (map html-elem->para body-contents)]
+    (js/console.log body-contents)
+    paragraphs))
+
+#_(js/Array.from (.-children (.-body (str->document test-file))))
+(html->doc test-file)
--- src/slate/macros.clj
+++ src/slate/macros.clj
@@ -1,6 +1,6 @@
 (ns slate.macros)
 
 (defmacro slurp-file
-  "Loads a file's whole contents and returns a string, at compile time. "
+  "Loads a file's whole contents and returns a string, at compile time (filepath is relative to project root)."
   [filepath]
   (slurp (java.io.File. filepath)))
