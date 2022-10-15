33998cf4e9b3539d046ebacea5a065cd04140c21
Jack Arrington <mail@jackarrington.com>
2022-10-15 16:26:02 -0400

    Generate a big ole random paragraph.

---
+++ src/dev/README.md
@@ -0,0 +1,3 @@
+## Dev-Related Namespaces
+
+The files in this folders are to be used for _development_ only, i.e. to be run at the REPL. They should never be depended on by code outside of this folder.
--- src/drop/app/components/slate_editor.cljs
+++ src/drop/app/components/slate_editor.cljs
@@ -19,6 +19,7 @@
   (some-> ui-state :history (history/current-state) :doc))
 
 (declare *slate-instance)
+(when utils/DEV (set! js/globalSlateInstance *slate-instance))
 (def *full-screen? (r/atom false))
 (defonce *open-file (doto (persistent-atom ::open-file
                                            {:path nil, :last-saved-doc nil}
